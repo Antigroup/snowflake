a86281076c7c04be171a5c931b1b5107a4b02b84
Thoren Paulson <tdpaulson@live.com>
2015-10-23 23:00:54 -0500

    Added channel preference options instead of choosing YELL all the time.

--- Snowflake/Snowflake.lua
+++ Snowflake/Snowflake.lua
@@ -22,6 +22,10 @@ function Snowflake:ADDON_LOADED(arg1)
 	if SnowflakeMessageTable == nil then
 		SnowflakeMessageTable = {}
 	end
+    if SnowflakeChatPreferences == nil then
+        SnowflakeChatPreferences = {}
+        SnowflakeChatPreferences._all = 'YELL'
+    end
 end
 
 function Snowflake:SlashHandler(msg, editbox)
@@ -39,8 +43,13 @@ function Snowflake:SlashHandler(msg, editbox)
 end
 
 function Snowflake:list(args)
+    print('Global channel preference: '..SnowflakeChatPreferences._all)
     for k,v in pairs(SnowflakeMessageTable) do
-        print(k..': '..v)
+        if SnowflakeChatPreferences[k] == nil then
+            print(k..': '..v)
+        else
+            print(k..' ('..SnowflakeChatPreferences[k]..'): '..v)
+        end
     end
 end
 
@@ -66,6 +75,9 @@ function Snowflake:help(args)
     print('Snowflake: Adds an entry to yell <message> when raid member named <name> dies. Also used to change message for existing entries.')
     print('Snowflake: /snowflake remove <name>')
     print('Snowflake: Remove entry for player named <name>.')
+    print('Snowflake: /snowflake setchannel <channel> [name]')
+    print('Snowflake: Set the channel to use for message, globally or for player named <name>. ')
+    print('Snowflake: <channel> can be one of {SAY, EMOTE, YELL, PARTY, GUILD, RAID, RAID_WARNING, INSTANCE_CHAT, BATTLEGROUND, NIL}. NIL removes preferences for specific players.')
     print('Snowflake: /snowflake list')
     print('Snowflake: List all names with their associated messages.')
 end
@@ -74,9 +86,35 @@ function Snowflake:test(args)
     self:UnitKilled(args)
 end
 
+function Snowflake:setchannel(args)
+    channel, name = args:match('^(%S*)%s*(.-)$')
+    
+    if channel ~= nil then
+        if channel == 'SAY' or channel == 'EMOTE' or channel == 'YELL' or channel == 'PARTY' or channel == 'GUILD' or channel == 'RAID' or channel == 'RAID_WARNING' or channel == 'INSTANCE_CHAT' or channel == 'BATTLEGROUND' or channel == 'NIL' then
+            if channel ~= 'NIL' then
+                if name == '' or name == nil then
+                    SnowflakeChatPreferences._all = channel
+                    print('Snowflake: Set global channel preference to '..channel..'.')
+                else
+                    SnowflakeChatPreferences[name] = channel
+                    print('Snowflake: Set channel preference for '..name..' to '..channel)
+                end
+            else
+                SnowflakeChatPreferences[name] = nil
+                print('Snowflake: Removed channel preference for '..name)
+            end
+        end
+    end
+end
+
 function Snowflake:UnitKilled(name)
     if SnowflakeMessageTable[name] ~= nil  then
-        SendChatMessage(SnowflakeMessageTable[name], 'YELL', self.language, nil)
+        channel = SnowflakeChatPreferences._all
+        if SnowflakeChatPreferences[name] ~= nil then
+            channel = SnowflakeChatPreferences[name]
+        end
+        
+        SendChatMessage(SnowflakeMessageTable[name], channel, self.language, nil)
     end
 end
 
